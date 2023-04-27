ef07f2ed4dfe8ad667d64552178afe5e0a40e998
MajesticSoftware <jeff.scruggs23@gmail.com>
2023-04-27 04:21:02 -0400

    InitialCommit

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ function App() {
     return (
         <div className={styles.App}>
             <header className={styles['App-header']}>
+                <img src="https://wixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg" />
                 <img src={logo} className={styles['App-logo']} alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
                 <a
                     className={styles['App-link']}
                     href="https://reactjs.org"
